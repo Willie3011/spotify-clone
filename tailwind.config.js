0dcaa84a9af48d6add89a15e97d14f515f27743f
Buddynice34 <willemapril33@gmail.com>
2024-06-08 09:34:23 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        spotifyGreen: '#1DB954',
+        spotifyBlack: '#191414',
+        spotifyDarkGray: '#121212',
+        spotifyLightGray: '#B3B3B3',
+      },
+    },
+  },
+  plugins: [],
+}
+
